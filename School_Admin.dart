5ad82fdd0efea0bc4444094c7702bf962d0d701d
Ahmedbhai2023 <ahmedrazamanjothi29@gmail.com>
2025-01-12 14:18:07 -0800

    Change

--- School_Admin.dart
+++ School_Admin.dart
@@ -18,16 +18,40 @@ void main() {
   // Sign Up
 
  String SignUp = '';
+ String Email = '';
+ String Passward = '';
  
   while (true) {
     print('This is for ${position} Enter SignUp You');
     SignUp = stdin.readLineSync()!;  
 
     if (SignUp == 'signup' || SignUp == 'SignUp') {
-      print('You : $SignUp');
+      print('Generate Your $position Email');
+      Email = stdin.readLineSync()!;
+       print('Generate Your $position Passward');
+      Passward = stdin.readLineSync()!;
       break;  
     } else {
       print('Invalid input. Please Again Enter SignUp');
+    }
+  }
+
+  //Login 
+    String Login = '';
+
+  while (true) {
+    print('This is for ${position} Enter LogIn You');
+    Login = stdin.readLineSync()!;  
+ String LoginEmail = '';
+ String LoginPassward = '';
+        if (Login == 'LogIn' || Login == 'login') {
+      print('Enter Your $position Email');
+      LoginEmail = stdin.readLineSync()!;
+       print('Enter Your $position Passward');
+      LoginPassward = stdin.readLineSync()!;
+      break;  
+    } else {
+      print('Invalid input. Please enter "Teacher" or "Student".');
     }
   }
 
